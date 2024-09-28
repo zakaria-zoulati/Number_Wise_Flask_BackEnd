19c2ad758f9c4e2e67b9efa4896ac9f2e72dc1fa
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-28 17:41:53 +0200

    done

---
+++ Algos/DoublyTriangular.java
@@ -0,0 +1,5 @@
+public class DoublyTriangular {
+    public static void main( String[] args ){
+        int n = 999 ; 
+    }
+}
---
+++ Algos/Triangular.java
@@ -0,0 +1,5 @@
+public class Triangular {
+    public static void main( String[] args ){
+        int n = 999 ; 
+    }
+}
