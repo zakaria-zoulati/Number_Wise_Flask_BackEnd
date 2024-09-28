89d535ee44222c1ebbe672ca903d7af7e8026be5
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-28 16:31:45 +0200

    done

---
+++ Algos/EvenOdd.java
@@ -0,0 +1,9 @@
+public class EvenOdd {
+    public void check( int n ){
+        if( n % 2 == 0 ){
+            System.out.println("The number n  is Even") ; 
+        }else {
+            System.out.println("The number n is even") ;
+        }
+    }
+}
---
+++ Algos/Prime.java
@@ -0,0 +1,28 @@
+public class Prime {
+    public boolean isPrim(int n) {
+        if (n < 2) {
+            return false; 
+        }
+        if (n == 2 || n == 3) {
+            return true; 
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false; 
+        }
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int n = 113;
+        Prime p = new Prime();
+        if (p.isPrim(n)) {
+            System.err.println("Yes, the number " + n + " is prime");
+        } else {
+            System.err.println("The number " + n + " is not prime");
+        }
+    }
+}
