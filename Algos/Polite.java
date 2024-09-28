91d9a0550a0bc7be313f3758774c49dc4c60a9b4
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-28 19:49:24 +0200

    done

---
+++ Algos/Polite.java
@@ -0,0 +1,12 @@
+public class Polite {
+    public static boolean isPolite( int n  ){
+        if( ( n & ( n-1 ) ) == 0  ) return false ; 
+        return true ; 
+    }
+    public static void main(String[] args){
+        int n = 4 ; 
+        if( isPolite(n) ){
+            System.out.println(n+" Is a Polite number") ; 
+        }
+    }
+}
