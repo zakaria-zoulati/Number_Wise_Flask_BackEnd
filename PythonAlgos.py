331396d8e6e4a62f834e0be250bc68539432c444
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-30 00:02:56 +0200

    done

--- PythonAlgos.py
+++ PythonAlgos.py
@@ -7,6 +7,16 @@ def is_prime(n):
             return False
     return True
 
+
+#For Unit Testing Purpose
+def test_is_prime() : 
+    assert is_prime(7) == True 
+    assert is_prime(21) == False 
+    assert is_prime(0) == False  
+    assert is_prime(0) == False  
+    assert is_prime(100003) == True  
+
+
 # To cheks Either The Number is Palindromic or Not  . 
 def check_is_palindromic(n) :
     original = str(n)
@@ -14,5 +24,11 @@ def check_is_palindromic(n) :
     return  original == reversed_str 
 
 
+# For Unit Testing Purpose
+def test_is_palindromic() : 
+    assert check_is_palindromic(121) == True
+    assert check_is_palindromic(12321) == True
+    assert check_is_palindromic(123) == False
+    assert check_is_palindromic(-121) == False
 
         
