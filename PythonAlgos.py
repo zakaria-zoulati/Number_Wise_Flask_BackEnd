1e7a9215c5bf0a0a96bed870deedc102ce061c42
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-30 22:49:52 +0200

    done

--- PythonAlgos.py
+++ PythonAlgos.py
@@ -4,6 +4,9 @@ To simply Checkouts The Unit Tests Yiu can Just Run :
 
 """
 
+import math 
+
+############################################################################################################################
 # Cheks Either a number is prime or not .
 def is_prime(n):
     if n <= 1:
@@ -22,6 +25,7 @@ def test_is_prime() :
     assert is_prime(0) == False  
     assert is_prime(100003) == True  
 
+###########################################################################################################################
 
 # To cheks Either The Number is Palindromic or Not  . 
 def check_is_palindromic(n) :
@@ -36,5 +40,29 @@ def test_is_palindromic() :
     assert check_is_palindromic(12321) == True
     assert check_is_palindromic(123) == False
     assert check_is_palindromic(-121) == False
+
+###########################################################################################################################
+
+# Function to check if a number is a perfect square
+def is_perfect_square(x):
+    s = int(math.sqrt(x))
+    return s * s == x
+
+# Function to check if a number is a Fibonacci number
+def is_fibonacci(n):
+    if( n <=0  ) : 
+        return False 
+    return is_perfect_square(5 * n * n + 4) or is_perfect_square(5 * n * n - 4)
+
+
+
+def test_fibonacci() : 
+    assert is_fibonacci(5) == True 
+    assert is_fibonacci(2) == True 
+    assert is_fibonacci(-1) == False 
+    assert is_fibonacci(44444) == False 
+    assert is_perfect_square(100) == True  
+    assert is_perfect_square(99) == False
+    assert is_perfect_square(3) == False
 
         
